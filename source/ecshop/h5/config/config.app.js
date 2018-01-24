9ad18428915b3651885b00a44994c76b9cb79314
BoWang <ralwayne@163.com>
2018-01-24 12:15:23 +0800

    网站配置

--- source/ecshop/h5/config/config.app.js
+++ source/ecshop/h5/config/config.app.js
@@ -1,8 +1,8 @@
 var GLOBAL_CONFIG = {
-	'API_HOST': 'https://api.ecshop36.geek-zoo.net',
-	'APP_NAME': 'Demo',
-	'APP_DESC': 'This is a demo',
-	'APP_KEYWORDS': 'Demo, Shop',
+	'API_HOST': 'https://api.terncloud.com',
+	'APP_NAME': 'dsh',
+	'APP_DESC': 'This is a dsh',
+	'APP_KEYWORDS': '滴水湖,农业园,Shop',
 	'DEBUG': false,
 	'ENCRYPTED':false,
 	 //如果公众号没有配置，将for_weixin置为false，才能在微信中使用
